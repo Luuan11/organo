a55179fa30700ae920f2f36552ae08c84587e366
Luuan11 <luancraft55@gmail.com>
2023-12-16 23:52:34 -0300

    :tada: new course

---
+++ src/components/CampoText/CampoText.js
@@ -0,0 +1,14 @@
+import './CampoText.css'
+
+const CampoText = () => {
+    return (
+        <div className="campo-text">
+            <label>Nome</label>
+            <input 
+            placeholder='Digite seu nome'
+            />
+        </div>
+    )
+}
+
+export default CampoText
