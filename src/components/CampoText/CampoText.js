b65d511a33d0c365ae2e59bc22149c5c734ba752
Luuan11 <luancraft55@gmail.com>
2023-12-19 23:46:28 -0300

    :sparkles: added the change

--- src/components/CampoText/CampoText.js
+++ src/components/CampoText/CampoText.js
@@ -2,12 +2,17 @@ import './CampoText.css'
 
 const CampoText = (props) => {
 
+    const onType = (event) => {
+        valor = event.target.value
+    }
+
     const placeholderMod = `${props.placeholder}...`
 
     return (
         <div className="campo-text">
             <label>{props.label}</label>
             <input 
+            onChange={onType}
             placeholder={placeholderMod}
             required={props.required}
             />
