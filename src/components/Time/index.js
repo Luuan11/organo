8baa75fe11c1d09937bc6d4494982ec0ec54bc9a
Luuan11 <luancraft55@gmail.com>
2024-01-17 22:56:31 -0300

    :sparkles: created the function changedColor

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,20 +1,23 @@
 import Colaborador from "../Colaborador"
+import hexToRgba from "hex-to-rgba"
+
 import "./Time.css"
 
-const Time = ({ time, colaboradores, aoDeletar }) => {
-    const corSegundaria =  { backgroundColor: time.corSegundaria }
-    const corPrimaria = { borderColor: time.corPrimaria }
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
+    
+    const hexRgba = {backgroundColor: hexToRgba(time.cor, '0.6')}
+    // const corPrimaria = { borderColor: time.corPrimaria }
 
     return (
-        colaboradores.length > 0) && <section className="time" style={corSegundaria}>
-            <input type="color" className="input-changeColor" />
-            <h3 style={corPrimaria}>{time.nome}</h3>
+        colaboradores.length > 0) && <section className="time" style={hexRgba}>
+            <input value={time.cor} onChange={event => mudarCor(event.target.value, time.nome)} type="color" className="input-changeColor" />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
             {/* <div className="deletar"> */}
                 {colaboradores.map((colaborador, indice) => 
                 <Colaborador key={indice} 
                 colaborador={colaborador} 
-                corDeFundo={time.corSecundaria} 
+                corBackground={time.cor}
                 aoDeletar ={aoDeletar} 
 
                 />
