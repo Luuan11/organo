55389c1f8b8f875512e5d314d60010a185327196
Luuan11 <luancraft55@gmail.com>
2024-01-17 23:16:01 -0300

    testing

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,6 +1,5 @@
 import Colaborador from "../Colaborador"
-import hexToRgba from "hex-to-rgba";
-
+import hexToRgba from 'hex-to-rgba';
 import "./Time.css"
 
 const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
